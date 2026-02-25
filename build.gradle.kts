9555c6d340ba3be3c72dc7d3dbcbfdde4d95e783
Daniel Garnier-Moiroux <git@garnier.wf>
2026-02-25 21:34:35 +0100

    build: update jreleaser to use central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jreleaser.model.Active
 
 plugins {
-    id("org.jreleaser") version "1.20.0"
+	id("org.jreleaser") version "1.22.0"
 }
 
 group = "wf.garnier"
@@ -10,44 +10,38 @@ version = "4.0.0"
 // Required for jreleaser, see:
 // https://github.com/jreleaser/jreleaser/issues/1492
 tasks {
-    create("clean") {}
+	register("clean") {}
 }
 
-jreleaser {
-    gitRootSearch.set(true)
-    signing {
-        active = Active.ALWAYS
-        armored = true
-    }
-    deploy {
-        maven {
-            nexus2 {
-                create("testcontainers-dex") {
-                    active.set(Active.ALWAYS)
-                    url = "https://s01.oss.sonatype.org/service/local"
-                    snapshotUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                    closeRepository = true
-                    releaseRepository = true
-                    stagingRepository("testcontainers-dex/build/staging-deploy")
-                    transitionMaxRetries = 100
-                }
-                create("spring-boot-testcontainers-dex") {
-                    active.set(Active.ALWAYS)
-                    url = "https://s01.oss.sonatype.org/service/local"
-                    snapshotUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                    closeRepository = true
-                    releaseRepository = true
-                    stagingRepository("spring-boot-testcontainers-dex/build/staging-deploy")
-                    transitionMaxRetries = 100
-                }
 
-            }
-        }
-    }
-    release {
-        github {
-            repoOwner = "Kehrlann"
-            overwrite = true
-        }
-    }
+jreleaser {
+	gitRootSearch.set(true)
+	signing {
+		pgp {
+			active = Active.ALWAYS
+			armored = true
+		}
+	}
+	deploy {
+		maven {
+			mavenCentral {
+				create("testcontainers-dex") {
+					active.set(Active.ALWAYS)
+					url = "https://central.sonatype.com/api/v1/publisher"
+					stagingRepository("testcontainers-dex/build/staging-deploy")
+				}
+				create("spring-boot-testcontainers-dex") {
+					active.set(Active.ALWAYS)
+					url = "https://central.sonatype.com/api/v1/publisher"
+					stagingRepository("spring-boot-testcontainers-dex/build/staging-deploy")
+				}
+			}
+		}
+	}
+	release {
+		github {
+			repoOwner = "Kehrlann"
+			overwrite = true
+		}
+	}
 }
